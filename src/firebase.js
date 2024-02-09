67ee3efdd782001b4f2b787487c0be08fcd3031d
Channa-123 <96125389+Channa-123@users.noreply.github.com>
2024-02-09 20:38:05 +0530

    create firebase

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+import firebase from "firebase/app";
+import "firebase/database";
+const firebaseConfig = {
+    apiKey: "AIzaSyDSvaykm9bWGxEqvEdt1-7ym15dOAyIeJc",
+    authDomain: "contact-form-36bde.firebaseapp.com",
+    projectId: "contact-form-36bde",
+    storageBucket: "contact-form-36bde.appspot.com",
+    messagingSenderId: "238890185696",
+    appId: "1:238890185696:web:4e75da4e42348050c63185"
+  };
